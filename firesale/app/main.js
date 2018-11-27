22d44360395a7ba3fb8ea3ded7e548e01c91d833
zhangrenjian <570543941@qq.com>
2018-11-27 16:09:37 +0800

    feat: markdown preview app add edit and preview func

---
+++ firesale/app/main.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require('electron');
+let mainWindow = null;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow();
+  mainWindow.loadFile(`${__dirname}/index.html`);
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
