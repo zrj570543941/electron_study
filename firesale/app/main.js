7959f43fae2ee83f18775fb0c1bd5cffe6d8872a
zhangrenjian <570543941@qq.com>
2018-11-30 16:51:13 +0800

    1.feat: markdown preview app add waring on existing window when there are unsaved changes 2.feat: markdown preview app add warning on files displayed on the markdown preview app has changed when other app has edited and saved the file 3.feat: markdown preview app add waring on opening other files when current file has changed 4.feat: markdown preview app add drag and drop file to open

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, dialog } = require('electron');
 const fs = require('fs')
 
 const windows = new Set();
+const openFiles = new Map();
 let mainWindow = null;
 
 const getFileFromUser = exports.getFileFromUser  = (targetWindow) => {
@@ -21,6 +22,7 @@ const openFile = (targetWindow, file) => {
   app.addRecentDocument(file);
   targetWindow.setRepresentedFilename(file);
   targetWindow.webContents.send('file-opened', file, content);
+  startWatchingFile(targetWindow, file)
 };
 
 const createWindow = exports.createWindow = () => {
@@ -31,7 +33,25 @@ const createWindow = exports.createWindow = () => {
   });
   newWindow.on('closed', () => {
     windows.delete(newWindow);
+    stopWatchingFile(newWindow);
     newWindow = null;
+  });
+  newWindow.on('close', (event) => {
+    if (newWindow.isDocumentEdited()) {
+      event.preventDefault();
+      const result = dialog.showMessageBox(newWindow, {
+        type: 'warning',
+        title: 'Quit with Unsaved Changes?',
+        message: 'Your changes will be lost if you do not save.',
+        buttons: [
+          'Quit Anyway',
+          'Cancel',
+        ],
+        defaultId: 0,
+        cancelId: 1
+      });
+      if (result === 0) newWindow.destroy();
+    }
   });
   windows.add(newWindow);
   return newWindow;
@@ -66,6 +86,33 @@ const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
   fs.writeFileSync(file, content);
   // 若没有打开文件，而直接新建后想保存，就在保存完后在window title上更新为相应的文件名
   openFile(targetWindow, file);
+};
+
+const startWatchingFile = (targetWindow, file) => {
+  stopWatchingFile(targetWindow);
+  console.log(2)
+  // todo:watch不生效
+  // const watcher = fs.watchFile(file, (cur, prev)=> {
+  //   // console.log(event)
+  //   // if (cur.mtimeMs !== prev.mtimeMs) {
+  //     console.log(1)
+  //     const content = fs.readFileSync(file).toString();
+  //     targetWindow.webContents.send('file-changed', file, content);
+  //   // }
+  // });
+  const watcher = fs.watch(file, (event)=> {
+    if (event === 'change') {
+      const content = fs.readFileSync(file).toString();
+      targetWindow.webContents.send('file-changed', file, content);
+    }
+  });
+  openFiles.set(targetWindow, watcher);
+};
+const stopWatchingFile = (targetWindow) => {
+  if (openFiles.has(targetWindow)) {
+    openFiles.get(targetWindow).stop();
+    openFiles.delete(targetWindow);
+  }
 };
 
 app.on('ready', () => {
--- firesale/app/renderer.js
+++ firesale/app/renderer.js
@@ -28,10 +28,26 @@ const updateUserInterface = (isEdited) => {
   if (filePath) { title = `${path.basename(filePath)} - ${title}`; }
   if (isEdited) { title = `${title} (Edited)`; }
   currentWindow.setTitle(title);
+  // 设置当前窗口的文档是否被编辑过了，用于后续在关闭窗口时获取一种状态————当前窗口是否编辑过
   currentWindow.setDocumentEdited(isEdited);
   saveMarkdownButton.disabled = !isEdited;
   revertButton.disabled = !isEdited;
 };
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+const fileTypeIsSupported = (file) => {
+  return ['text/plain', 'text/markdown'].includes(file.type);
+};
+
+const renderFile = (file, content) => {
+  filePath = file;
+  originalContent = content;
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+  updateUserInterface(false);
+};
+const isDifferentContent = (content) => content !== markdownView.value;
+
 markdownView.addEventListener('keyup', (event) => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
@@ -41,12 +57,40 @@ markdownView.addEventListener('keyup', (event) => {
 openFileButton.addEventListener('click', () => {  mainProcess.getFileFromUser(currentWindow);});
 
 ipcRenderer.on('file-opened', (event, file, content) => {
-  filePath = file
-  originalContent = content
+  if (currentWindow.isDocumentEdited()) {
+    const result = remote.dialog.showMessageBox(currentWindow, {
+      type: 'warning',
+      title: 'Overwrite Current Unsaved Changes?',
+      message: 'Opening a new file in this window will overwrite your unsaved changes. Open this file anyway?',
+      buttons: [
+      'Yes',
+      'Cancel',
+    ],
+      defaultId: 0,
+      cancelId: 1
+  });
+    if (result === 1) { return; }
+  }
+  renderFile(file, content);
+});
+
+ipcRenderer.on('file-changed', (event, file, content) => {
+  if (!isDifferentContent(content)) return;
+  const result = remote.dialog.showMessageBox(currentWindow, {
+    type: 'warning',
+    title: 'Overwrite Current Unsaved Changes?',
+    message: 'Another application has changed this file. Load changes?',
+    buttons: [
+      'Yes',
+      'Cancel',
+    ],
+    defaultId: 0,
+    cancelId: 1
+  });
+  if (result === 0) {
+    renderFile(file, content);
+  }
   
-  markdownView.value = content;
-  renderMarkdownToHtml(content);
-  updateUserInterface(false);
 });
 
 newFileButton.addEventListener('click', () => {
@@ -64,4 +108,37 @@ saveMarkdownButton.addEventListener('click', () => {
 revertButton.addEventListener('click', () => {
   markdownView.value = originalContent;
   renderMarkdownToHtml(originalContent);
+});
+
+document.addEventListener('dragstart', event => event.preventDefault());
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('dragleave', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
+
+markdownView.addEventListener('dragover', (event) => {
+  const file = getDraggedFile(event);
+  
+  if (fileTypeIsSupported(file)) {
+    markdownView.classList.add('drag-over');
+  } else {
+    markdownView.classList.add('drag-error');
+  }
+});
+
+markdownView.addEventListener('dragleave', () => {
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
+});
+
+markdownView.addEventListener('drop', (event) => {
+  const file = getDroppedFile(event);
+  
+  if (fileTypeIsSupported(file)) {
+    mainProcess.openFile(currentWindow, file.path);
+  } else {
+    alert('That file type is not supported');
+  }
+  
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
 });
